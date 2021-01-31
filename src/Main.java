d5c0960229813ab6f6140ec4e547633312775a20
derrj <j.derriks@hva.nl>
2021-01-31 19:52:50 +0100

    eerste opzet

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// standaardcode IntelliJ. Gebruik het voor testjes e.d.
+        System.out.println("Hello from Automata & Programming. Choose the right class to run!");
+    }
+}
---
+++ src/week1/FSAexec.java
@@ -0,0 +1,36 @@
+package week1;
+
+/**
+ * TODO: fix it and learn to use JAVADOC and use it!
+ */
+public class FSAexec {
+
+    public static void main(String[] args) {
+        // TODO: ask for input and create better output
+        fsaExec("ababaaabac");
+
+    }
+
+    /**
+     * TODO: Analyse the code and make it so it returns something useful. Fix the javadoc
+     *
+     * @param input
+     */
+
+    public static void fsaExec(String input) {
+        boolean[] acceptState = {false, false, false, true};
+
+        int[][] followUpAlphabetStates = {
+                {0, 1}, // state [0] (followUpAlphabetStates[[0]['a'] = 0 en [0]['b']=1 enz
+                {0, 2}, // state [1]
+                {0, 3}, // state [2]
+                {3, 3}  // state [3]
+        };
+        int state = 0;
+        for (int i = 0; i < input.length(); i++) {
+            state = followUpAlphabetStates[state][input.charAt(i) - 'a'];
+        }
+        System.out.println(acceptState[state]);
+    }
+
+}
